11c6ea8b8ca001a01549923e50c58b896467aa04
Sameer Shaikh <sameer291@yahoo.co.in>
2025-12-19 23:28:36 +0530

    added

--- Parent Module/main.tf
+++ Parent Module/main.tf
@@ -3,3 +3,23 @@ module "resource_group" {
   resource_group_name     = "rg_01"
   resource_group_location = "centralindia"
 }
+
+
+module "VNET" {
+  depends_on = [ module.resource_group ]
+  source                   = "../child Module/VNET"
+  virtual_network_name     = "todo_vnet"
+  virtual_network_location = "centralindia"
+  resource_group_name      = "todo_app"
+  address_space            = ["10.0.0.0/16"]
+
+}
+
+module "subnet" {
+  depends_on = [ module.VNET ]
+  source               = "../child Module/subnet"
+  subnet_name          = "frontend_subnet"
+  resource_group_name  = "todo_app"
+  virtual_network_name = "todo_vnet"
+  address_prefixes     = ["10.0.1.0/24"]
+}
